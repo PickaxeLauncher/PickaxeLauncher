4b1837ce7ef6e3bf39a2eb0c6ba383c26978696f
Cameron Dehning <bedsteler2.0@gmail.com>
2023-10-17 14:38:32 -0500

    _

--- src/Views/NewInstanceDialog.cs
+++ src/Views/NewInstanceDialog.cs
@@ -1,7 +1,9 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Nickvision.GirExt;
 using Pickaxe.Controllers;
+using Pickaxe.Helpers;
 using Pickaxe.Models;
 
 namespace Pickaxe.Views;
@@ -13,7 +15,8 @@ public class NewInstanceDialog : Adw.Window {
     [Gtk.Connect] private readonly Gtk.CheckButton _forge_check = null;
     [Gtk.Connect] private readonly Gtk.CheckButton _fabric_check = null;
     [Gtk.Connect] private readonly Adw.EntryRow _name_entry = null;
-    [Gtk.Connect] private readonly Gtk.Button _change_icon_button = null;
+    [Gtk.Connect] private readonly Gtk.Button _change_icon_button;
+    [Gtk.Connect] private readonly Gtk.Picture _icon = null;
 
     public NewInstanceDialog(NewInstanceController controller) : this(
         new Gtk.Builder("new_instance_dialog.ui"), controller) {
@@ -21,17 +24,33 @@ public class NewInstanceDialog : Adw.Window {
 
     private NewInstanceDialog(Gtk.Builder builder, NewInstanceController controller) : base(
         builder.GetPointer("_root"), false) {
-        _controller = controller;
         builder.Connect(this);
+        _controller = controller;
         Modal = true;
         _change_icon_button.OnClicked += ChangeIcon;
     }
 
-    private void ChangeIcon(Gtk.Button sender, EventArgs args) => Task.Run(async () => {
-        var picker = Gtk.FileDialog.New();
-        var file = picker.OpenAsync(this);
-        if (file is null) return;
-        await _controller.ChangeIcon(file);
-    });
+    private void ChangeIcon(Gtk.Button sender, EventArgs args) {
+        var _icon_picker = Gtk.FileDialog.New();
+        var filter = Gtk.FileFilter.New();
+        filter.AddPixbufFormats();
+        _icon_picker.SetDefaultFilter(filter);
+        const int ICON_SIZE = 100;
+        var task = _icon_picker.OpenAsync(this);
+        task.GetAwaiter().OnCompleted(() => {
+            var file = task.Result;
+            if (file == null) return;
+            GdkPixbuf.Pixbuf pixbuf = GdkPixbuf.Pixbuf.NewFromFile(file.GetPath());
+
+            if (pixbuf.Width != pixbuf.Height) {
+                int smallestSide = Math.Min(pixbuf.Width, pixbuf.Height);
+                pixbuf = pixbuf.NewSubpixbuf(0, 0, smallestSide, smallestSide);
+            }
+            if (pixbuf.Width != ICON_SIZE && pixbuf.Height != ICON_SIZE) {
+                pixbuf = pixbuf.ScaleSimple(ICON_SIZE, ICON_SIZE, GdkPixbuf.InterpType.Bilinear);
+            }
+            _icon.SetPixbuf(pixbuf);
+        });
+    }
 
 }
